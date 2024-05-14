ebc02e805ba71db46828c1402583c15be4f7b0d6
Jagpreet25700 <jagpreetsingh25700@gmail.com>
2024-05-14 01:16:48 -0400

    Create Udemy_python.py

---
+++ python repository/Udemy_python.py
@@ -0,0 +1,5 @@
+print ("we will help you to provide your brand name. To continue, please fill the information below")
+City_name = input("Please provide you City name: ")
+Pet_name = input("Please provide your pet name(If applicable): ")
+
+print("The suggested name for your band is: "+City_name+" "+Pet_name)
